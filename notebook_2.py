d455eee1e22a028faa50774ee93cbe616a1a3098
g2a-world <g2a-world>
2023-10-24 02:11:36 +0000

    protection rule

--- notebook_2.py
+++ notebook_2.py
@@ -1,2 +1,7 @@
 # Databricks notebook source
+# MAGIC %md
+# MAGIC #### The protection rules apply to all including adminstrators.
+
+# COMMAND ----------
+
 print("checking the protection of the main barnch")
