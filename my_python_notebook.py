64fba0d5870d25bfd8851209ca438add3e2cb13c
g2a-world <g2a-world>
2023-10-24 01:08:05 +0000

    adding comments

--- my_python_notebook.py
+++ my_python_notebook.py
@@ -1,3 +1,12 @@
 # Databricks notebook source
+# MAGIC %md
+# MAGIC #### Protecting the main branch
+
+# COMMAND ----------
+
 print("Hello World")
 print("Greetings Hello World")
+
+# COMMAND ----------
+
+
