df511f3e4b8f76c184c1a5599db4eeb429a696a5
juneeyuabi@gmail.com <juneeyuabi@gmail.com>
2023-10-28 03:23:28 +0000

    commit changes

--- notebook_4.py
+++ notebook_4.py
@@ -4,3 +4,17 @@ print("my third notebook")
 print("my fourth notebook")
 print("my fith notebook")
 print("my seventh notebook")
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC
+# MAGIC ###This is updating the same notebook by two diffrent users to check merge confilict.
+
+# COMMAND ----------
+
+print("my third notebook")
+
+print("my fourth notebook")
+print("my fith notebook")
+print("my seventh notebook")
