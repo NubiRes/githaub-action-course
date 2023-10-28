a03e8a094a27e3bef42e598c878066f8a8dbcf8b
g2a-world <g2a-world>
2023-10-28 03:11:02 +0000

    commit

--- notebook_4.py
+++ notebook_4.py
@@ -4,3 +4,17 @@ print("my third notebook")
 print("my fourth notebook")
 print("my fith notebook")
 print("my seventh notebook")
+
+# COMMAND ----------
+
+# MAGIC %fs ls dbfs:/mnt/formula1dlg2a/processed/constructors/
+
+# COMMAND ----------
+
+df = spark.read.parquet("/mnt/formula1dlg2a/processed/constructors/")
+
+display(df)
+
+# COMMAND ----------
+
+
